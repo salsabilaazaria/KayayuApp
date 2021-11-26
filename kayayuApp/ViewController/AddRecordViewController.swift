269fec6354b60ff9f4bde39cedcbce0e1dc70c41
Salsabila Azaria <i-salsabila.azaria@dana.id>
2021-11-26 11:08:05 +0700

    add record node

---
+++ kayayuApp/ViewController/AddRecordViewController.swift
@@ -0,0 +1,8 @@
+//
+//  AddRecordViewController.swift
+//  kayayuApp
+//
+//  Created by Salsabila Azaria on 25/11/21.
+//
+
+import Foundation
---
+++ kayayuApp/Views/Add Record/AddRecordeNode.swift
@@ -0,0 +1,8 @@
+//
+//  AddRecordeNode.swift
+//  kayayuApp
+//
+//  Created by Salsabila Azaria on 25/11/21.
+//
+
+import Foundation
