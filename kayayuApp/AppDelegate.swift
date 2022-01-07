e8462a25c219ad376b66d0cc8cc65b8919219a6a
Salsabila Azaria <salsabilaazariaa@gmail.com>
2022-01-08 00:10:28 +0700

    adding IQKeyboardManager

--- kayayuApp/AppDelegate.swift
+++ kayayuApp/AppDelegate.swift
@@ -8,13 +8,14 @@
 import UIKit
 import CoreData
 import Firebase
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
-
+		IQKeyboardManager.shared.enable = true
 		// Override point for customization after application launch.
 		return true
 	}
